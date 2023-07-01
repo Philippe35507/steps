d64883ae8502c435213a1b78076a673f7be92903
Philippe35507 <phdefoy@gmail.com>
2023-07-01 18:20:39 +0100

    Setting style conditionally

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const step = 3;
   return (
     <div className="steps">
       <div className="numbers">
-        <div className="active">1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
 
       <p className="message">
